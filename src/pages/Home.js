63eb41cda647362938c4a8ead68e39bd7f360197
Sigit Kuncoro <sigit.kunc@gmail.com>
2020-10-19 02:25:03 +0700

    fix: rename home page

---
+++ src/pages/Home.js
@@ -0,0 +1,47 @@
+import React from "react"
+import { useStaticQuery } from "gatsby"
+import { get } from "lodash"
+
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+
+const GetDetailPage = graphql`
+  query GetDetailPage {
+    allMarkdownRemark(
+      filter: { id: { eq: "d57e5e9a-5510-5e64-b736-463d03fbba84" } }
+    ) {
+      nodes {
+        id
+        frontmatter {
+          title
+          slug
+          description
+          list
+        }
+        html
+      }
+    }
+  }
+`
+
+const HomePage = ({ id }) => {
+  const data = useStaticQuery(GetDetailPage)
+
+  const title = get(data, "allMarkdownRemark.nodes[0].frontmatter.title", "")
+  const description = get(
+    data,
+    "allMarkdownRemark.nodes[0].frontmatter.description",
+    ""
+  )
+  const skills = get(data, "allMarkdownRemark.nodes[0].frontmatter.list", "")
+  const content = get(data, "allMarkdownRemark.nodes[0].html", "")
+
+  return (
+    <Layout>
+      <SEO title={title} description={description} />
+      <div dangerouslySetInnerHTML={{ __html: content }} />
+    </Layout>
+  )
+}
+
+export default HomePage
